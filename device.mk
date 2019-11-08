12a7fdabb6f1453bcde5a5c2c11c2d8f650183c1
Elektroschmock <elektroschmock78@googlemail.com>
2019-11-08 12:14:38 +0100

    fugu: Move fstab to vendor/etc
    Change-Id: I876af8e03b8f5e7f9318b200c19841d0dfd070e1

--- device.mk
+++ device.mk
@@ -215,7 +215,7 @@ PRODUCT_PACKAGES += \
 
 ## Ramdisk
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/fstab.fugu:root/fstab.fugu \
+    $(LOCAL_PATH)/rootdir/fstab.fugu:$(TARGET_OUT_VENDOR)/etcfstab.fugu \
     $(LOCAL_PATH)/rootdir/init.fugu.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.fugu.rc \
     $(LOCAL_PATH)/rootdir/init.fugu.countrycode.sh:system/bin/init.fugu.countrycode.sh \
     $(LOCAL_PATH)/rootdir/init.fugu.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.fugu.usb.rc \
