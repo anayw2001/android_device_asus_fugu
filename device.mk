faeed451c7e434d232397b11fb0d4ddaabc0b1b5
Anay Wadhera <awadhera@berkeley.edu>
2021-04-25 13:58:19 -0700

    add back libaudiopolicymanager target

--- device.mk
+++ device.mk
@@ -45,8 +45,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-impl
 
 ## Audio Policy
-#USE_CUSTOM_AUDIO_POLICY := 1
-#USE_LEGACY_AUDIO_POLICY := 1
+USE_CUSTOM_AUDIO_POLICY := 1
+USE_LEGACY_AUDIO_POLICY := 1
 USE_XML_AUDIO_POLICY_CONF := 1
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
--- libaudio/Android.mk
+++ libaudio/Android.mk
@@ -95,3 +95,45 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -Werror
 
 include $(BUILD_SHARED_LIBRARY)
+
+
+##################################
+# Audio Policy Manager
+##################################
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    ATVAudioPolicyManager.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libcutils \
+    liblog \
+    libutils \
+    libmedia \
+    libatv_audio \
+    libbinder \
+    libaudiopolicymanagerdefault
+
+LOCAL_C_INCLUDES := \
+    external/tinyalsa/include \
+    $(TOPDIR)frameworks/av/services/audiopolicy \
+    $(TOPDIR)frameworks/av/services/audiopolicy/common/include \
+    $(TOPDIR)frameworks/av/services/audiopolicy/common/managerdefinitions/include \
+    $(TOPDIR)frameworks/av/services/audiopolicy/engine/interface
+
+ifneq ($(filter fugu fugu_gmscore_next, $(TARGET_PRODUCT)),)
+LOCAL_C_INCLUDES += \
+    vendor/google_athome/services/RemoteControlService/include
+
+LOCAL_SHARED_LIBRARIES += \
+    libremotecontrolservice
+
+LOCAL_CFLAGS += -DREMOTE_CONTROL_INTERFACE
+endif
+
+LOCAL_MODULE := libaudiopolicymanager
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_CFLAGS += -Werror -g
+
+include $(BUILD_SHARED_LIBRARY)
